b16c4ab14d89ff336a7dd0fd82edc63e7a591102
Oliver Portee <oliver.portee@gmail.com>
2020-01-02 11:54:50 +0100

    remove useless http status check in get and auth saga

--- frontend/src/actions/sagas/auth.js
+++ frontend/src/actions/sagas/auth.js
@@ -7,22 +7,13 @@ import { READ_AUTH_TOKEN_SUCCESS, READ_AUTH_TOKEN_FAILURE } from '../../globals/
 export default function* authToken(payload) {
   try {
     let response = yield call(getAuthToken, payload.args)
-    if (response.status === 401 || response.status === 404) {
-      throw Object.assign(
-        new Error(),
-        { status: response.status }
-      );
-    } else if (response.status === 200) {
-      yield put({
-        type: READ_AUTH_TOKEN_SUCCESS,
-        payload: {
-          ...response.payload,
-          status: response.status
-        }
-      })
-    } else {
-      alert("Unexpected API status code ")
-    }
+    yield put({
+      type: READ_AUTH_TOKEN_SUCCESS,
+      payload: {
+        ...response.payload,
+        status: response.status
+      }
+    })
   } catch (error) {
     yield put({
       type: READ_AUTH_TOKEN_FAILURE,
