c4ef33f64c03ed3f4b2233d894848b2aaf97be4f
Oliver Portee <oliver.portee@gmail.com>
2020-01-03 15:03:07 +0100

    create functions for choosing the right saga

--- frontend/src/actions/sagas/index.js
+++ frontend/src/actions/sagas/index.js
@@ -3,10 +3,10 @@ import { takeEvery } from 'redux-saga/effects'
 import { actionTypes } from 'redux-resource'
 
 import getUsers from './getUsers'
-import postUser from './postUser'
-import deleteUser from './deleteUser'
 import getFiles from './getFiles'
+import postUser from './postUser'
 import postFile from './postFile'
+import deleteUser from './deleteUser'
 import deleteFile from './deleteFile'
 
 import authTokenSaga from './auth'
@@ -14,6 +14,36 @@ import { READ_AUTH_TOKEN_PENDING } from '../../globals/actionTypes'
 
 export default function* rootSaga() {
   yield takeEvery(READ_AUTH_TOKEN_PENDING, authTokenSaga)
+
+  yield takeEvery((request) => (
+    request.type === actionTypes.READ_RESOURCES_PENDING
+    && request.resourceType === 'users'
+  ), getUsers)
+
+  yield takeEvery((request) => (
+    request.type === actionTypes.READ_RESOURCES_PENDING
+    && request.resourceType === 'files'
+  ), getFiles)
+
+  yield takeEvery((request) => (
+    request.type === actionTypes.CREATE_RESOURCES_PENDING
+    && request.resourceType === 'users'
+  ), postUser)
+
+  yield takeEvery((request) => (
+    request.type === actionTypes.CREATE_RESOURCES_PENDING
+    && request.resourceType === 'files'
+  ), postFiles)
+
+  yield takeEvery((request) => (
+    request.type === actionTypes.DELETE_RESOURCES_PENDING
+    && request.resourceType === 'users'
+  ), deleteUser)
+
+  yield takeEvery((request) => (
+    request.type === actionTypes.DELETE_RESOURCES_PENDING
+    && request.resourceType === 'files'
+  ), deleteFile)
 }
 
 
