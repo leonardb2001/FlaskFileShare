98deda94aeb2d1ddc026dfa7936de13b3f601986
Oliver Portee <oliver.portee@gmail.com>
2020-01-08 20:55:07 +0100

    add test users route, comment on curl examples

--- backend/api/test.py
+++ backend/api/test.py
@@ -1,16 +1,17 @@
 
 from api import app
-from flask import request, abort
-from flask_httpauth import HTTPBasicAuth
-import json
+from flask import request, abort, jsonify
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+
+from api import testdata
 
 login = HTTPBasicAuth()
-auth = HTTPBasicAuth(scheme='Token')
+auth = HTTPTokenAuth(scheme='Bearer')
 prefix = '/test'
 
 @login.verify_password
-def verify_password(userid, password):
-    if userid == 'abcdefgh123456789' and password == 'password123':
+def verify_password(username, password):
+    if username == 'tommy' and password == 'password123':
         return True
     return False
 
@@ -20,12 +21,15 @@ def verify_token(token):
         return True
     return False
 
+# curl -i tommy:password123@localhost:5000/test/auth_token
 @app.route(prefix + '/auth_token')
 @login.login_required
 def auth_token():
     return 'secret_auth_token'
 
 
+# curl -H "Authorization: Bearer secret_auth_token" -i localhost:5000/test/users?username=tommy
+# curl -H "Authorization: Bearer secret_auth_token" -d '{"username":"u","email":"e","password":"p"}' -H "Content-Type: application/json" -i localhost:5000/test/users
 @app.route(prefix + '/users', methods=['GET', 'POST'])
 @auth.login_required
 def getUsers():
@@ -34,7 +38,17 @@ def getUsers():
         if username == None:
             abort(400)
         else:
-            pass
-
-
+            return jsonify(testdata.users)
+    if request.method == 'POST':
+        if (not request.json or
+            not 'username' in request.json or
+            not 'email' in request.json or
+            not 'password' in request.json):
+            abort(400)
+        username = request.json.get('username')
+        email = request.json.get('email')
+        password = request.json.get('password')
+        if username == 'tommy':
+            abort(403)
+        return (jsonify({'id': 'aosien20n29na0sd9r3n20'}), 201)
 
---
+++ backend/api/testdata.py
@@ -0,0 +1,25 @@
+
+
+users = [
+    {
+      'id': '9e32f25dab6c4d7f8bd54a4bfba9ccd9',
+      'username': 'tommy',
+      'email': 'tommy@gmail.com'
+    },
+    {
+      'id': 'a562b2f7d5584499bd87b724ba534940',
+      'username': 'anna',
+      'email': 'ana-jason@web.de'
+    },
+    {
+      'id': '25c429337a2f4b8ca7476f9cd2724f88',
+      'username': 'golden_dragon',
+      'email': 'kevin.steinke.1996@gmx.de'
+    },
+    {
+      'id': '06034d5db6f14ed98758e477d34487a6',
+      'username': 'frankchicken',
+      'email': 'frankfrankfrank@t-online.de'
+    }
+  ]
+
