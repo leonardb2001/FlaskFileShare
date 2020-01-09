1a7183ff4fba36991542e0fa1bd615b3bfbfa3a7
Oliver Portee <oliver.portee@gmail.com>
2020-01-09 19:18:43 +0100

    add routes

--- frontend/src/react/routes.js
+++ frontend/src/react/routes.js
@@ -12,15 +12,41 @@ import DeleteFileExample from './examples/DeleteFileExample'
 
 const routes = [
   {
+    path: '/',
+    exact: true,
+    component: () => <h1>home</h1>,
+    private: false
+  },
+  {
     path: '/login',
     exact: true,
-    component: <>login</>,
+    component: () => <h1>login</h1>,
     private: false
+  },
+  {
+    path: '/register',
+    exact: true,
+    component: () => <h1>register</h1>,
+    private: false
+  },
+  {
+    path: '/users/:username/:folder*',
+    exact: true,
+    component: () => <h1>files</h1>,
+    private: false // actually true
+  },
+  {
+    path: '/user-search/:search_text',
+    exact: true,
+    component: () => <h1>search results</h1>,
+    private: false // actually true
+  },
+  {
+    path: '/upload',
+    exact: true,
+    component: () => <h1>upload</h1>,
+    private: false // actually true
   }
-
-
-
-
 ]
 
 export default routes
