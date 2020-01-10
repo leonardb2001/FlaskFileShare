eb3dd1f784103630017b84a7e97bed1babb38bc6
Oliver Portee <oliver.portee@gmail.com>
2020-01-10 15:56:21 +0100

    send login request to server with axios

--- frontend/src/actions/sagas/auth.js
+++ frontend/src/actions/sagas/auth.js
@@ -1,19 +1,25 @@
 
 import { put, call } from 'redux-saga/effects'
+import axios from 'axios'
 
-import {
-  getAuthToken200,
-//  getAuthToken401
-} from '../../testData/auth'
 import { READ_AUTH_TOKEN_SUCCESS, READ_AUTH_TOKEN_FAILURE } from '../../globals/actionTypes'
+
+const DOMAIN = 'http://localhost:5000'
 
 export default function* authToken(payload) {
   try {
-    let response = yield call(getAuthToken200)
+    let response = yield call(axios.get, DOMAIN + '/test/auth_token', {
+      auth: {
+        username: 'tommy',
+        password: 'password123'
+      }
+    })
     yield put({
       type: READ_AUTH_TOKEN_SUCCESS,
       payload: {
-        ...response.payload,
+        username: response.data.username,
+        auth_token: response.data.auth_token,
+        userid: response.data.userid,
         status: response.status
       }
     })
