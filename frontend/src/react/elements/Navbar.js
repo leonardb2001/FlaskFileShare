3fc97c95a2a022ba791a7e0427d402c58f54f4a1
Oliver Portee <oliver.portee@gmail.com>
2020-01-11 15:58:31 +0100

    add login and register buttons

--- frontend/src/react/elements/Navbar.js
+++ frontend/src/react/elements/Navbar.js
@@ -1,18 +1,36 @@
 
 import React from 'react'
+import { connect } from 'react-redux'
 
+import { Link } from 'react-router-dom'
 import {
   AppBar,
-  Toolbar
+  Button,
+  ButtonGroup,
+  Toolbar,
+  withStyles
 } from '@material-ui/core'
+
+const styles = {
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-end'
+  }
+}
 
 class Navbar extends React.Component {
   render() {
+    const { isAuthenticated, classes } = this.props
     return (
       <>
-        <AppBar>
-          <Toolbar>
-
+        <AppBar position='static'>
+          <Toolbar className={classes.toolbar}>
+            { !isAuthenticated &&
+            <ButtonGroup variant='contained' color='secondary'>
+              <Button component={Link} to='/login'>login</Button>
+              <Button component={Link} to='/register'>register</Button>
+            </ButtonGroup>
+            }
           </Toolbar>
         </AppBar>
       </>
@@ -20,4 +38,10 @@ class Navbar extends React.Component {
   }
 }
 
-export default Navbar
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: state.auth.isAuthenticated
+  }
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(Navbar))
