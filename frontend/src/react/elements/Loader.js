6799c0c8fea45c6f64e90186eade2f7e81fdba26
Oliver Portee <oliver.portee@gmail.com>
2020-01-11 18:18:27 +0100

    add Loader

---
+++ frontend/src/react/elements/Loader.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+import {
+  Box,
+  CircularProgress
+} from '@material-ui/core'
+
+export default () => (
+  <Box m={2} display='flex' justifyContent='center' alignItems='center'>
+    <CircularProgress />
+  </Box>
+)
