3b4bc5bc391124ed5b0d76dca3ce0692d4684657
Oliver Portee <oliver.portee@gmail.com>
2020-01-12 19:54:37 +0100

    beautify database class

--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -1,6 +1,8 @@
 
 from app import app
+from app.database import db
 
 @app.route('/')
 def index():
+    db.test()
     return 'hello world'
--- backend/app/database/__init__.py
+++ backend/app/database/__init__.py
@@ -1,4 +1,4 @@
 
-from database import Database
+from app.database.database import Database
 
 db = Database()
