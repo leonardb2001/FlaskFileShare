f037215930f416cb5b11bf541cfe745e263276d8
Oliver Portee <oliver.portee@gmail.com>
2020-01-13 22:27:45 +0100

    add __eq__ on user class

--- backend/app/model/user.py
+++ backend/app/model/user.py
@@ -21,3 +21,10 @@ class User:
     
     def toJSON(self):
         pass
+
+    def __eq__(self, other):
+        return self.uuid == other.uuid and
+            self.uname == other.uname and
+            self.email == other.email and
+            self.pw_hash == other.pw_hash and
+            self.date == other.date
