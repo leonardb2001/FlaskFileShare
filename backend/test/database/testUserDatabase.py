8da40979d78cc2973dc6a295f5403b01642f1d56
Oliver Portee <oliver.portee@gmail.com>
2020-01-13 23:43:05 +0100

    add unit tests

--- backend/test/database/testUserDatabase.py
+++ backend/test/database/testUserDatabase.py
@@ -1,6 +1,7 @@
 
 import unittest
 import os
+import sqlite3
 from app.database.database import Database
 from app.model import User
 
@@ -8,9 +9,7 @@ file = 'ffs_test.db'
 
 class DatabaseTest(unittest.TestCase):
     def setUp(self):
-        if os.path.exists(file):
-            os.remove(file)
-        self.db = Database(file)
+        self.db = Database(file, force=True)
 
     def testAddUser(self):
         user = User(
@@ -34,3 +33,58 @@ class DatabaseTest(unittest.TestCase):
         self.db.addUser(user1)
         user2 = self.db.getUser(uuid)
         self.assertEqual(user1, user2)
+
+    def testUniqueUname(self):
+        user1 = User(
+             '1',
+             'user',
+             'user1@email.com',
+             'oawornv0awe9fh9efhn9en',
+             1578950353
+        )
+        user2 = User(
+             '2',
+             'user',
+             'user2@email.com',
+             'oawornv0awe9fh9efhn9en',
+             1578950353
+        )
+        self.db.addUser(user1)
+        with self.assertRaises(sqlite3.IntegrityError):
+            self.db.addUser(user2)
+
+    def testUniqueEmail(self):
+        user1 = User(
+             '1',
+             'user1',
+             'user@email.com',
+             'oawornv0awe9fh9efhn9en',
+             1578950353
+        )
+        user2 = User(
+             '2',
+             'user2',
+             'user@email.com',
+             'oawornv0awe9fh9efhn9en',
+             1578950353
+        )
+        self.db.addUser(user1)
+        with self.assertRaises(sqlite3.IntegrityError):
+            self.db.addUser(user2)
+
+    def testWrongUseridReturnsNone(self):
+        self.assertIsNone(self.db.getUser('wrong_id'))
+
+    def testDeleteUser(self):
+        uuid = '1'
+        user = User(
+             uuid,
+             'user',
+             'user1@email.com',
+             'oawornv0awe9fh9efhn9en',
+             1578950353
+        )
+        self.db.addUser(user)
+        self.db.deleteUser(uuid)
+        self.assertIsNone(self.db.getUser(uuid))
+
