51fcd685536e644f6558b3461dbf81a792bace23
Oliver Portee <oliver.portee@gmail.com>
2020-01-13 22:27:30 +0100

    add unittest for database

---
+++ backend/test/database/testUserDatabase.py
@@ -0,0 +1,18 @@
+
+import unittest
+from app.database.database import Database
+from app.model import User
+
+class DatabaseTest(unittest.Testcase):
+    def __init__(self):
+        self.db = Database('ffs_test.db')
+
+    def testAddGetUser(self):
+        uuid = '123456789'
+        user1 = User(uuid, 'uname', 'uname@email.com', 'oawornv0awe9fh9efhn9en', 1578950353)
+        self.db.addUser(user1)
+        user2 = self.db.getUser(uuid)
+        self.assertEqual(user1, user2)
+
+
+
